92d6c2dd5d88f05dd3e47d7a19e1cdc91e8b9e59
Namratha85 <147474276+Namratha85@users.noreply.github.com>
2023-10-10 11:08:18 +0530

    Add files via upload

---
+++ ArithmetiOperator.java
@@ -0,0 +1,33 @@
+package OPERATORS;
+//a+b
+//-,+,*,%,/
+
+public class ArithmetiOperator {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=20;
+		int c=a+b;
+		System.out.println("addition of two numbers"+c);
+		
+		int e=100;
+		int f=20;
+		int g=e-f;
+		System.out.println("subtraction of two numbers"+g);
+		
+		int r=100;
+		int n=20;
+		int p=r*n;
+		System.out.println("multiplication of two numbers"+p);
+		
+		int s=100;
+		int j=20;
+		int l=s/j;
+		System.out.println("Division of two numbers"+l);
+		
+		int k=625;
+		int v=5;
+		int q=k%v;
+		System.out.println("percentage of two numbers"+q);
+	}
+}
